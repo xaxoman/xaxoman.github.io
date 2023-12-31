8a90dbabc466bed8fc8d9289a3feeef432fc15d9
xaxoman <xaxoman@users.noreply.github.com>
2023-12-31 17:54:32 +0100

    Added transition of the elements on scroll using the js intersection observer

--- main.js
+++ main.js
@@ -5,3 +5,26 @@
 
 
 
+/* GLOBE END*/ 
+
+// Website transitions on scroll using intersection observer
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      
+      }
+      
+      else {   /* To apply the class multiple times if the condition is true */ 
+  
+              entry.target.classList.remove('show');
+      }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden');
+  console.log('Hidden Elements:', hiddenElements);
+  hiddenElements.forEach((el) => observer.observe(el))
+
