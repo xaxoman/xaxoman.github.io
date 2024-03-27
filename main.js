28070b4e2330d96b427e1a01ecc8de650e6c96fd
xaxoman <dennisx.fjdv8@silomails.com>
2024-03-27 20:36:58 +0100

    performance 3 (added partytown js library)

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
+import { init } from '@builder.io/partytown';
 
-// Website transitions on scroll using intersection observer
+// Initialize Partytown
+init();
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       console.log(entry)
       if (entry.isIntersecting) {
           entry.target.classList.add('show');
-      
       }
       
     /*  else {   // To apply the class multiple times if the condition is true 
-  
               entry.target.classList.remove('show');
       } */
     });
-  });
+});
   
-  const hiddenElements = document.querySelectorAll('.hidden');
-  console.log('Hidden Elements:', hiddenElements);
-  hiddenElements.forEach((el) => observer.observe(el))
-
+const hiddenElements = document.querySelectorAll('.hidden');
+console.log('Hidden Elements:', hiddenElements);
+hiddenElements.forEach((el) => observer.observe(el))
