a765ce94a331e94d94b360ab8c4347fa4880fb9e
xaxoman <dennisx.fjdv8@silomails.com>
2024-09-02 12:12:54 +0200

    fished the translation process, everything works. i had pproblems with creating new lines with the \n escape caracter but i modif the script to translate it to a <br> tag

--- main.js
+++ main.js
@@ -39,3 +39,41 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+/* SCRIPT FOR THE CHANGING OF THE LANGUAGE */
+document.getElementById('language-select').addEventListener('change', function() {
+  const selectedLanguage = this.value;
+  loadTranslations(selectedLanguage);
+});
+
+function loadTranslations(language) {
+  fetch(`locales/${language}.json`)
+      .then(response => response.json())
+      .then(translations => {
+          updateLanguage(translations);
+      })
+      .catch(error => console.error('Error loading translations:', error));
+}
+
+function updateLanguage(translations) {
+  document.querySelectorAll('[data-key]').forEach(element => {
+      const key = element.getAttribute('data-key');
+      const keys = key.split('.');
+      let translation = translations;
+      keys.forEach(k => {
+          translation = translation[k];
+      });
+     
+      // Replace \n with <br> to create line breaks
+      if (translation) {
+        translation = translation.replace(/\n/g, '<br>');
+    }
+
+    element.innerHTML = translation; // Use innerHTML to render HTML elements like <br>
+    
+  });
+}
+
+// Set default language
+loadTranslations('en');
