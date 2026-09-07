2217501793727f7f75af89b3b0fd15303605b889
Denis Xhafaj <denis.xhafaj@atumtek.it>
2026-09-07 17:54:49 +0200

    fix(footer): remove Wake for Fajr privacy link from bottom footer bar

--- components/footer.tsx
+++ components/footer.tsx
@@ -115,11 +115,6 @@ export default function Footer() {
           <span>{t("footer.copyright").replace("{year}", new Date().getFullYear().toString())}</span>
         </ClientOnly>
         <ClientOnly>
-          <Link href="/privacy/wake-for-fajr" style={{ color: "var(--dim)", fontSize: 13 }}>
-            {t("footer.privacy.wakeForFajr")}
-          </Link>
-        </ClientOnly>
-        <ClientOnly>
           <span>{t("footer.location")}</span>
         </ClientOnly>
       </div>
